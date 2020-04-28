4dc491b46e963ded053771088b2464a9e0d315fe
matevzgolavsek <matevz.golavsek@gmail.com>
2020-04-28 12:41:00 +0200

    Add snapshot tests and fix CSS for tests

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,2 @@
-require('@orange-ui/orange/dist/css/orange.css');
+// require('@orange-ui/orange/dist/css/orange.css');
+import '@orange-ui/orange/dist/css/orange.css';
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  transformIgnorePatterns: ["/node_modules/(?!(@storybook/.*\\.vue$))"],
+  preset: "@vue/cli-plugin-unit-jest",
+
+  // need to add this for Unit testing purposes otherwise there are CSS issues
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy'
+  }
 };
